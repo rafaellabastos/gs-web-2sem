0f0a1119cab57eca127c4b045b22075a767ed513
LuizFFarias <rm99519@fiap.com.br>
2023-11-21 17:44:03 -0300

    funcionalidade de login criada através do consumo de API

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,37 @@
+"use client"
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Login() {
+    const [usuario, setUsuario] = useState("");
+    const [senha, setSenha] = useState("");
+    const [usuarioCadastrado, setUsuarioCadastrado] = useState([]);
+    const [alerta, setAlerta] = useState("")
+
+    const handleChange = ((e) => {
+        e.preventDefault()
+        fetch(`http://localhost:8080/imunocheck/${usuario}`, {
+            method: "get"
+        }).then((resp) => {
+            return resp.json()
+        }).then((resp) => {
+            setUsuarioCadastrado(resp)
+            console.log(usuarioCadastrado)
+        }).catch((error) =>{
+            console.log("Não encontrado")
+            console.log(error)
+        })
+        handleSubmit()
+    })
+    const handleSubmit = (() => {
+        if( usuarioCadastrado.find((userCadastrado) => userCadastrado.usuario == usuario && userCadastrado.senha == senha)){
+            setAlerta("Usuário encontrado!")
+        }
+        else{
+            setAlerta("Usuário e/ou senha incorretos!")
+        }
+    })
+
     return (
         <>
             <div className='cabecalhoTitulo'>
@@ -13,17 +44,18 @@ export default function Login() {
                 <form>
                     <br/><div>
                         <label htmlFor="username">Usuário:</label><br/>
-                        <input type="text" id="username"/>
+                        <input type="text" id="username" onChange={(e) => setUsuario(e.target.value)}/>
                     </div>
         
                     <br/><div>
                         <label htmlFor="password">Senha:</label><br/>
-                        <input type="password" id="password"/>
+                        <input type="password" id="password" onChange={(e) => setSenha(e.target.value)}/>
                     </div>
         
                     <div>
-                        <button type="submit">Entrar</button>
+                        <button type="submit" onClick={handleChange}>Entrar</button>
                     </div>
+                    {alerta}
                 </form>
             </div>
         </>
