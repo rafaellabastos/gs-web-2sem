dac515fb8414a73d8c249b4559bda0c6d146b506
LuizFFarias <rm99519@fiap.com.br>
2023-11-21 21:06:05 -0300

    funcionalidades de cadastro realizado e arrumei o erro do login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,32 +5,29 @@ import { useState } from 'react'
 export default function Login() {
     const [usuario, setUsuario] = useState("");
     const [senha, setSenha] = useState("");
-    const [usuarioCadastrado, setUsuarioCadastrado] = useState([]);
     const [alerta, setAlerta] = useState("")
 
-    const handleChange = ((e) => {
-        e.preventDefault()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
         fetch(`http://localhost:8080/imunocheck/${usuario}`, {
             method: "get"
-        }).then((resp) => {
-            return resp.json()
-        }).then((resp) => {
-            setUsuarioCadastrado(resp)
-            console.log(usuarioCadastrado)
-        }).catch((error) =>{
-            console.log("Não encontrado")
-            console.log(error)
         })
-        handleSubmit()
-    })
-    const handleSubmit = (() => {
-        if( usuarioCadastrado.find((userCadastrado) => userCadastrado.usuario == usuario && userCadastrado.senha == senha)){
-            setAlerta("Usuário encontrado!")
-        }
-        else{
-            setAlerta("Usuário e/ou senha incorretos!")
-        }
-    })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            const usuarioCadastrado = resp;
+            if (usuarioCadastrado.find((userCadastrado) => userCadastrado.usuario === usuario && userCadastrado.senha === senha)) {
+                setAlerta("Usuário encontrado!");
+                console.log(usuarioCadastrado)
+            } else {
+                setAlerta("Usuário e/ou senha incorretos!");
+            }
+        })
+        .catch((error) => {
+            console.log("Não encontrado");
+            console.log(error);
+        });
+    };
 
     return (
         <>
@@ -41,7 +38,7 @@ export default function Login() {
             <h2 className="tituloLogin">LOGIN</h2>
 
             <div className="cardLogin">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <br/><div>
                         <label htmlFor="username">Usuário:</label><br/>
                         <input type="text" id="username" onChange={(e) => setUsuario(e.target.value)}/>
@@ -53,7 +50,7 @@ export default function Login() {
                     </div>
         
                     <div>
-                        <button type="submit" onClick={handleChange}>Entrar</button>
+                        <button type="submit">Entrar</button>
                     </div>
                     {alerta}
                 </form>
