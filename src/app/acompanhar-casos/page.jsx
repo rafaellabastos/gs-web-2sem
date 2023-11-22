fdff790a86749a168b0e1fb5f18d04ac2f58103e
LuizFFarias <rm99519@fiap.com.br>
2023-11-22 11:47:25 -0300

    tentativa de criar um contador de vacinas por estado

--- src/app/acompanhar-casos/page.jsx
+++ src/app/acompanhar-casos/page.jsx
@@ -5,6 +5,44 @@
     export default function AcompanharCasos() {
         const [loading, setLoading] = useState(false);
         const [mostrar, setMostrar] = useState(false); 
+        const [estado, setEstado] = useState("");
+        const [dados, setDados] = useState({
+            estado: "",
+            id: ""
+        })
+        const [id, setId] = useState("")
+        const contador = 2
+        console.log(estado)
+
+        const handleChange = ((event) => {
+            setEstado(event.target.value)
+            const dadosEstados = estados.find(item => item.nome === estado)
+            const idVac = dadosEstados ? dadosEstados.id
+
+            setId(idVac.id)
+            console.log(id)
+
+        })
+        const handleSubmit = (() => {
+            fetch(`http://localhost:8080/imunocheck/vacinas`, {
+                method: "get"
+            }).then((resp) => resp.json())
+            .then((resp) => {
+                const tabelaVac = resp;
+                setDados((prevDados) => ({
+                       ...prevDados,
+                    estado: estado,
+                    id : id
+                }))
+                const estadoVac = tabelaVac.filter(item => item.estado === estado)
+                tabelaVac.find((idVacina) => setId(idVacina.id) )
+                if (estadoVac == estado && id ) {
+                    
+                } else {
+                    
+                }
+            })
+        })
 
         useEffect(() => {
             setLoading(false);
@@ -15,7 +53,7 @@
         }
 
         const estados = [
-            {nome: "Acre"},
+            {nome: "Acre", id: 1},
             {nome: "Alagoas"},
             {nome: "Amapá"},
             {nome: "Amazonas"},
@@ -66,7 +104,7 @@
 
                     <div>
                         <label htmlFor="state">Nome do estado:</label><br/>
-                        <select id="state" className='opcoesEstado'>
+                        <select id="state" className='opcoesEstado' onChange={handleChange}>
                             <option value="">Selecione um estado</option>
                             {estados.map(estado => (
                             <option key={estado.nome} value={estado.nome}>{estado.nome}</option>
@@ -95,126 +133,126 @@
                                 <td>1</td>
                                 <td>BCG</td>
                                 <td>Tuberculose</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>2</td>
                                 <td>Hepatite B</td>
                                 <td>Hepatite B</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>3</td>
                                 <td>Pentavalente</td>
                                 <td>DTP, Hib e HBV</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>4</td>
                                 <td>VIP/VOP</td>
                                 <td>Poliomelite</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>5</td>
                                 <td>Pneumocócica 10-valente</td>
                                 <td>Doenças pulmonares</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>6</td>
                                 <td>Meningicócica C</td>
                                 <td>Meningococo</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>7</td>
                                 <td>Rotavírus</td>
                                 <td>Rotavírus</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>8</td>
                                 <td>Tríplice viral</td>
                                 <td>Sarampo, caxumba e rubéola</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>9</td>
                                 <td>Hepatite A</td>
                                 <td>Hepatite A</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>10</td>
                                 <td>DTP</td>
                                 <td>Difteria, tétano e coqueluche</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>11</td>
                                 <td>Varicela</td>
                                 <td>Catapora</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>12</td>
                                 <td>Febre amarela</td>
                                 <td>Febre amarela</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>13</td>
                                 <td>HPV</td>
                                 <td>HPV</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>14</td>
                                 <td>Hepatite B</td>
                                 <td>Hepatite B</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>15</td>
                                 <td>Tríplice viral</td>
                                 <td>Sarampo, caxumba e rubéola</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>16</td>
                                 <td>Tríplice viral</td>
                                 <td>Sarampo, caxumba e rubéola</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>17</td>
                                 <td>Dupla adulto</td>
                                 <td>Difteria e tétano</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
 
                             <tr>
                                 <td>18</td>
                                 <td>Influenza</td>
                                 <td>Gripe</td>
-                                <td></td>
+                                <td>{contador}</td>
                             </tr>
                         </tbody>
                     </table>
